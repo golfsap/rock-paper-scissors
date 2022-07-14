35cf34e6e0e74fb8fe0956d41a57776c64b9ce2b
Golfsap <golf.sap@gmail.com>
2022-07-14 21:23:04 +0700

    Add game function to play 5 rounds and update player and computer score

--- game.js
+++ game.js
@@ -19,41 +19,96 @@ function playRound(playerSelection, computerSelection) {
 
     // check if the same
     if (playerChoice === computerSelection) {
-        return "It's a draw!";
+        console.log("It's a draw!");
+        return "Draw";
     }
 
     // if player chooses rock
     if (playerChoice === "Rock") {
         if (computerSelection === "Scissors") {
-            return "You Win! Rock beats Scissors";
+            console.log("You Win! Rock beats Scissors");
+            return "Win";
         }
         else {
-            return "You Lose! Paper beats Rock";
+            console.log("You Lose! Paper beats Rock");
+            return "Lose";
         }
     }
 
     // if player chooses scissors
     if (playerChoice === "Scissors") {
         if (computerSelection === "Paper") {
-            return "You Win! Scissors beats Paper";
+            console.log("You Win! Scissors beats Paper");
+            return "Win";
         }
         else {
-            return "You Lose! Rock beats Scissors";
+            console.log("You Lose! Rock beats Scissors");
+            return "Lose";
         }
     }
     
     // if player chooses paper
     if (playerChoice === "Paper") {
         if (computerSelection === "Rock") {
-            return "You Win! Paper beats Rock";
+            console.log("You Win! Paper beats Rock");
+            return "Win";
         }
         else {
-            return "You Lose! Scissors beats Paper";
+            console.log("You Lose! Scissors beats Paper");
+            return "Lose";
         }
     }
 }
 
-const playerSelection = "scissors";
-const computerSelection = computerPlay();
-console.log(computerSelection);
-console.log(playRound(playerSelection, computerSelection));
+function updateScore(winOrLose) {
+    // player score +1 if win, computer score +1 if lose, +1 both if draw
+    if (winOrLose === "Win") {
+        playerScore++;
+    }
+    else if (winOrLose === "Lose") {
+        computerScore++;
+    }
+    else {
+        playerScore++;
+        computerScore++;
+    }
+}
+
+function game() {
+    
+    // play 5 rounds in each game
+    for (let i = 0; i < 5; i++) {
+
+        // ask player for selection each round
+        let playerSelection = prompt("Rock, paper or scissors?");
+
+        // input player selection vs random computer selection
+        const computerSelection = computerPlay();
+        const roundResult = playRound(playerSelection, computerSelection);
+
+        // keep count of score
+        updateScore(roundResult);
+
+        // print current score
+        console.log(`Player score: ${playerScore}, Computer score ${computerScore}`);
+
+    }
+
+    // output winner of the game
+    if (playerScore > computerScore) {
+        console.log("You win the game!");
+    }
+    else if (playerScore < computerScore) {
+        console.log("You lost the game :(");
+    }
+    else {
+        console.log("It's a tie! :O");
+    }
+}
+
+// initialize starting score
+let playerScore = 0;
+let computerScore = 0;
+
+// start a game
+game();
