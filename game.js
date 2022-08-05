7cef69b6025c4b9d87c2e7582f3aeba4f70e62ec
Golfsap <golf.sap@gmail.com>
2022-08-05 12:49:06 +0700

    Add div to display player and computer score results

--- game.js
+++ game.js
@@ -13,9 +13,12 @@ function computerPlay() {
     return play;
 }
 
-function playRound(playerSelection, computerSelection) {
-    // // convert playerSelection to all lowercase with first letter capital
-    // const playerChoice = playerSelection.charAt(0).toUpperCase() + playerSelection.toLowerCase().slice(1);
+function playRound(e) {
+    const playerChoice = this.classList.value;
+    const computerSelection = computerPlay();
+
+    console.log(`You chose ${playerChoice}`);
+    console.log(`Computer chose ${computerSelection}`);
 
     // check if the same
     if (playerChoice === computerSelection) {
@@ -74,48 +77,47 @@ function updateScore(winOrLose) {
 }
 
 function game() {
-    
-    // // play 5 rounds in each game
-    // for (let i = 0; i < 5; i++) {
 
-    //     // ask player for selection each round
-    //     let playerSelection = prompt("Rock, paper or scissors?");
+    const buttons = Array.from(document.querySelectorAll('button'));
+    buttons.forEach(button => button.addEventListener('click', playRound));
 
-    //     // input player selection vs random computer selection
-    //     const computerSelection = computerPlay();
-    //     const roundResult = playRound(playerSelection, computerSelection);
+//     // input player selection vs random computer selection
+//     const roundResult = playRound(playerSelection, computerSelection);
 
-    //     // keep count of score
-    //     updateScore(roundResult);
+//     // keep count of score
+//     updateScore(roundResult);
 
-    //     // print current score
-    //     console.log(`Player score: ${playerScore}, Computer score ${computerScore}`);
+//     // print current score
+//     console.log(`Player score: ${playerScore}, Computer score ${computerScore}`);
 
-    // }
 
-    // output winner of the game
-    if (playerScore > computerScore) {
-        console.log("You win the game!");
-    }
-    else if (playerScore < computerScore) {
-        console.log("You lost the game :(");
-    }
-    else {
-        console.log("It's a tie! :O");
-    }
+
+//     // output winner of the game
+//     if (playerScore > computerScore) {
+//         console.log("You win the game!");
+//     }
+//     else if (playerScore < computerScore) {
+//         console.log("You lost the game :(");
+//     }
+//     else {
+//         console.log("It's a tie! :O");
+//     }
 }
 
-function logText(e) {
-    console.log(this.classList.value);
-}
 
-const buttons = Array.from(document.querySelectorAll('button'));
-buttons.forEach(button => button.addEventListener('click', logText));
-
+const playerContainer = document.querySelector('.player-score');
+const computerContainer = document.querySelector('.computer-score');
 
 // initialize starting score
 let playerScore = 0;
 let computerScore = 0;
 
+const pScore = document.createElement('p');
+pScore.textContent = playerScore;
+playerContainer.appendChild(pScore);
+
+const cScore = document.createElement('p');
+cScore.textContent = computerScore;
+computerContainer.appendChild(cScore);
 // start a game
 game();
