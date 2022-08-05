3ee33fdab867a066ab0ada63d89e2e908e3e42c9
Golfsap <golf.sap@gmail.com>
2022-08-05 10:53:01 +0700

    Add 3 buttons with classes and event listener to log text

--- game.js
+++ game.js
@@ -76,23 +76,23 @@ function updateScore(winOrLose) {
 
 function game() {
     
-    // play 5 rounds in each game
-    for (let i = 0; i < 5; i++) {
+    // // play 5 rounds in each game
+    // for (let i = 0; i < 5; i++) {
 
-        // ask player for selection each round
-        let playerSelection = prompt("Rock, paper or scissors?");
+    //     // ask player for selection each round
+    //     let playerSelection = prompt("Rock, paper or scissors?");
 
-        // input player selection vs random computer selection
-        const computerSelection = computerPlay();
-        const roundResult = playRound(playerSelection, computerSelection);
+    //     // input player selection vs random computer selection
+    //     const computerSelection = computerPlay();
+    //     const roundResult = playRound(playerSelection, computerSelection);
 
-        // keep count of score
-        updateScore(roundResult);
+    //     // keep count of score
+    //     updateScore(roundResult);
 
-        // print current score
-        console.log(`Player score: ${playerScore}, Computer score ${computerScore}`);
+    //     // print current score
+    //     console.log(`Player score: ${playerScore}, Computer score ${computerScore}`);
 
-    }
+    // }
 
     // output winner of the game
     if (playerScore > computerScore) {
@@ -105,6 +105,14 @@ function game() {
         console.log("It's a tie! :O");
     }
 }
+
+function logText(e) {
+    console.log(this);
+}
+
+const buttons = Array.from(document.querySelectorAll('button'));
+buttons.forEach(button => button.addEventListener('click', logText));
+
 
 // initialize starting score
 let playerScore = 0;
