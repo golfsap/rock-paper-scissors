5fe0763ee644ef852a2c93944fd9d346308379fe
Golfsap <golf.sap@gmail.com>
2023-07-21 11:38:15 +0700

    Add game functionality and UI

--- game.js
+++ game.js
@@ -1,128 +1,161 @@
-console.log("Hello there, this is rock paper scissors!");
-
-function computerPlay() {
-    // define array of choices
-    const choices = ["rock", "paper", "scissors"];
-
-    // get random index value
-    const randomIndex = Math.floor(Math.random() * choices.length);
-
-    // get random item
-    const play = choices[randomIndex];
-
-    return play;
+function getComputerChoice() {
+    // set array of choices
+    const options = ['Rock', 'Paper', 'Scissors'];
+    // get random index for computer choice
+    const computerChoice = options[Math.floor(Math.random()*options.length)];
+    return computerChoice;
 }
 
-function playRound(e) {
-    const playerChoice = this.classList.value;
-    const computerSelection = computerPlay();
+function playRound(playerSelection, computerSelection) {
+    // change to lowercase
+    const myChoice = playerSelection.toLowerCase();
+    const enemyChoice = computerSelection.toLowerCase();
 
-    // console.log(`You chose ${playerChoice}, Computer chose ${computerSelection}`);
-    logChoice(playerChoice, computerSelection);
-
-    // check if the same
-    if (playerChoice === computerSelection) {
-        console.log("It's a draw!");
-        return;
+    // define object if move is strong or weak against another
+    const weapons = {
+        rock: {weakTo: 'paper', strongTo: 'scissors'},
+        paper: {weakTo: 'scissors', strongTo: 'rock'},
+        scissors: {weakTo: 'rock', strongTo: 'paper'}
     }
 
-    if (((playerChoice ===  'rock') && (computerSelection === 'scissors')) || ((playerChoice ===  'paper') && (computerSelection === 'rock')) || ((playerChoice ===  'scissors') && (computerSelection === 'paper'))) {
-        console.log("You Win!");
-        updateScorePlayer();
+    if (weapons[myChoice].strongTo === enemyChoice) {
+        playerScore++;
+        console.log(`You Win! ${myChoice} beats ${enemyChoice}`);
+        roundWinner = "player";
+    }
+    else if (weapons[myChoice].weakTo === enemyChoice) {
+        computerScore++;
+        console.log(`You Lost! ${myChoice} is beaten by ${enemyChoice}`);
+        roundWinner =  'computer';
+    }
+    else {
+        console.log(`It's a Tie! ${myChoice} ties ${enemyChoice}`);
+        roundWinner = 'tie';
     }
 
-    else if (((playerChoice ===  'rock') && (computerSelection === 'paper')) || ((playerChoice ===  'paper') && (computerSelection === 'scissors')) || ((playerChoice ===  'scissors') && (computerSelection === 'rock'))) {
-        console.log('You Lose');
-        updateScoreCom();
-    }
-
+    updateScoreMsg(roundWinner, playerSelection, computerSelection);
+    updateSigns(playerSelection, computerSelection);
 }
 
-function updateScorePlayer() {
-    playerScore++;
-    pScore.textContent = playerScore;
-    if (playerScore == 5) {
-        const winnerMsg = document.createElement('p');
-        winnerMsg.classList.add('winner');
-        winnerMsg.textContent = 'Congratulations! You won!';
-        results.append(winnerMsg);
-        stopGame();
+function isGameOver() {
+    if (playerScore === 5 || computerScore === 5) {
+        return true;
     }
 }
 
-function updateScoreCom() {
-    computerScore++;
-    cScore.textContent = computerScore;
-    if (computerScore == 5) {
-        const winnerMsg = document.createElement('p');
-        winnerMsg.classList.add('winner');
-        winnerMsg.textContent = 'Sorry! You lost!';
-        results.append(winnerMsg);
-        stopGame();
+function printWinner() {
+    if (playerScore === 5) {
+        scoreInfo.innerText = "You won the game! :)";
+        console.log("You Won the game!");
     }
-}
-
-function logChoice(playerChoice, computerSelection) {
-    choices.textContent = `You chose ${playerChoice}, Computer chose ${computerSelection}`;
-}
-
-function stopGame() {
-    // create reset game button
-    const resetBtn = document.createElement('button');
-    resetBtn.classList.add('reset');
-    resetBtn.textContent = 'Reset Game';
-    results.appendChild(resetBtn);
-    resetBtn.addEventListener('click', resetGame);
-
-    // remove event listeners for choices
-    const buttons = Array.from(document.querySelectorAll('.choices button'));
-    buttons.forEach(button => button.removeEventListener('click', playRound));
-
-}
-
-function resetGame() {
-    playerScore = 0;
-    computerScore = 0;
-    pScore.textContent = playerScore;
-    cScore.textContent = computerScore;
-
-    const winner = document.querySelector('.winner');
-    winner.remove();
-    const choices = document.querySelector('#choices');
-    choices.textContent = '';
-    const resetBtn = document.querySelector('.reset');
-    resetBtn.remove();
-
-    game();
-
+    else {
+        scoreInfo.innerText = "You lost the game! :(";
+        console.log("You lost the game!");
+    }
 }
 
 function game() {
-
-    const buttons = Array.from(document.querySelectorAll('.choices button'));
-    buttons.forEach(button => button.addEventListener('click', playRound));
-
+    while (isGameOver() !== true) {
+        let playerSelection = prompt("What is your choice?");
+        playRound(playerSelection, getComputerChoice());
+        console.log(`Player score: ${playerScore} Computer score: ${computerScore}`);
+    }
+    printWinner();
 }
-
-
-const playerContainer = document.querySelector('.player-score');
-const computerContainer = document.querySelector('.computer-score');
-const results = document.querySelector('.results');
 
 // initialize starting score
 let playerScore = 0;
 let computerScore = 0;
+let roundWinner = '';
 
-const pScore = document.createElement('p');
-pScore.textContent = playerScore;
-playerContainer.appendChild(pScore);
+// UI elements
 
-const cScore = document.createElement('p');
-cScore.textContent = computerScore;
-computerContainer.appendChild(cScore);
+const playerScoreUI = document.getElementById("player-score");
+const computerScoreUI = document.getElementById("computer-score");
+const scoreInfo = document.getElementById("scoreInfo");
+const scoreMsg = document.getElementById("scoreMsg");
+const playerSign = document.getElementById("player-sign");
+const computerSign = document.getElementById("computer-sign");
 
-const choices = document.querySelector('#choices');
 
+function handleClick(playerSelection) {
+    if (isGameOver() === true) {
+        printWinner();
+        return
+    }
 
-// start a game
-game();
+    playRound(playerSelection, getComputerChoice());
+    console.log(`Player score: ${playerScore} Computer score: ${computerScore}`);
+    updateScore();
+
+    if (isGameOver() === true) {
+        printWinner();
+    }
+}
+
+function updateScore() {
+    playerScoreUI.innerText = `Player: ${playerScore}`;
+    computerScoreUI.innerText = `Computer: ${computerScore}`;
+    if (roundWinner === "player") {
+        scoreInfo.innerText = "You Win!";
+    }
+    else if (roundWinner === "computer") {
+        scoreInfo.innerText = "You Lost!";
+    }
+    else {
+        scoreInfo.innerText = "It's a Tie!";
+    }
+}
+
+function updateScoreMsg(winner, playerSelection, computerSelection) {
+    if (winner === 'player') {
+        scoreMsg.innerText = `${playerSelection} beats ${computerSelection}`;
+    }
+    else if (winner === 'computer') {
+        scoreMsg.innerText = `${playerSelection} is beaten by ${computerSelection}`;
+    }
+    else {
+        scoreMsg.innerText = `${playerSelection} ties ${computerSelection}`;
+    }
+}
+
+function updateSigns(playerSelection, computerSelection) {
+    switch (playerSelection) {
+        case "Rock": 
+            playerSign.innerText = "✊";
+            break;
+        case "Paper":
+            playerSign.innerText = "🖐️";
+            break;
+        case "Scissors":
+            playerSign.innerText = "✌️";
+            break;
+    }
+
+    switch (computerSelection) {
+        case "Rock": 
+            computerSign.innerText = "✊";
+            break;
+        case "Paper":
+            computerSign.innerText = "🖐️";
+            break;
+        case "Scissors":
+            computerSign.innerText = "✌️";
+            break;
+    }
+}
+
+// UI
+const weapons = document.querySelectorAll('button');
+weapons.forEach(weapon => {
+    weapon.addEventListener('click', (e) => {
+        //handleClick(weapon.className);
+        handleClick(weapon.id);
+    })
+});
+
+/*
+let playerScoreUI = document.querySelector(".player-score");
+console.log(playerScoreUI.innerText);
+playerScoreUI.innerText = "Player: " + playerScore;
+*/
